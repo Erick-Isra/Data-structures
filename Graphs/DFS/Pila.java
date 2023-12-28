79e24209c2b85c00016e3babda50032a6784e190
Erick Israel <88645031+Erick-Isra@users.noreply.github.com>
2023-12-28 00:07:58 -0600

    Add files via upload

---
+++ Graphs/DFS/Pila.java
@@ -0,0 +1,107 @@
+/**
+ * Implementación de la EDD Pila.
+ * @author Agapito Bautista Erick Israel
+ * @author Solano Juárez Sebastian
+ * @param <T> - El tipo de elementos de la Pila.
+ */
+public class Pila<T> {
+
+    /*
+    * Unidades básicas de una pila.
+    */
+    private class NodoPila {
+        /* Parámetros */
+        public T elemento;
+        public NodoPila siguiente;
+
+        /* Métodos */
+        /**
+         * Construye un NodoPila a partir de un elemento dado.
+         * @param elemento - El elemento que contendrá este NodoPila.
+         */
+        public NodoPila( T elemento ) {
+            this.elemento = elemento;
+            this.siguiente = null;
+        }
+    }
+
+    /* Parámetros */
+    private NodoPila cima;
+    private int longitud;
+
+    /* Métodos */
+    /**
+     * Crea la Pila vacía.
+     */
+    public Pila() {
+      this.cima = null;
+      this.longitud = 0;
+    }
+
+    /**
+     * Apila un nuevo elemento en esta Pila.
+     * @param elemento - El elemento a apilar.
+     */
+    public void apilar( T elemento ) {
+      NodoPila nuevo = new NodoPila(elemento);
+
+      if(longitud == 0){
+        cima = nuevo;
+        longitud++;
+      }else{
+        nuevo.siguiente = cima;
+        cima = nuevo;
+        longitud++;
+      }
+    }
+
+    /**
+     * Desapila el elemento de la cima de esta Pila.
+     * @throws Exception - Si esta Pila es vacía.
+     */
+    public void desapilar() throws Exception {
+
+      if(longitud == 0){
+        System.out.println("La pila es vacia");
+      }else{
+        NodoPila actual = cima;
+        cima = cima.siguiente;
+        actual.siguiente = null;
+        actual = null;
+        longitud--;
+      }
+    }
+
+    /**
+     * Devuelve el elemento contenido en la cima de esta Pila.
+     * @return La cima de esta Pila.
+     * @throws Exception - Si esta Pila es vacía.
+     */
+    public T darElementoCima() throws Exception {
+      if(longitud == 0){
+        return null;
+      }else{
+        return cima.elemento;
+      }
+    }
+
+    /**
+     * Determina si esta Pila es vacía.
+     * @return true si esta Pila es vacía. En caso contrario, false.
+     */
+    public boolean esVacia() {
+      if(longitud == 0){
+        return true;
+      }else{
+        return false;
+      }
+    }
+
+    /**
+     * Devuelve el número de elementos de esta Pila.
+     * @return El número de elementos de esta Pila.
+     */
+    public int darLongitud(){
+      return this.longitud;
+    }
+}
---
+++ Graphs/DFS/PruebaGrafica.java
@@ -0,0 +1,69 @@
+import java.util.LinkedList;
+/**
+ * Implementación PruebaGarfica
+ * @author Agapito Bautista Erick Israel
+ * @author Solano Juárez Sebastian
+ */
+/**Inicio de la clase PruebaGrafica */
+public class PruebaGrafica {
+
+    public static void main(String[] args) {
+        Grafica<Integer> g = new Grafica<>();
+        try {
+            g.insertarVertice("v1", 5);
+            g.insertarVertice("v2", 7);
+            g.insertarVertice("v3", 9);
+            g.insertarVertice("v4", 2);
+            g.insertarVertice("v5", 0);
+            g.insertarVertice("v6", 4);
+            g.insertarVertice("v7", 8);
+            g.insertarVertice("v8", 1);
+            g.insertarVertice("v9", 3);
+            g.insertarVertice("v10", 6);
+            g.insertarArista("v1", "v3");
+            g.insertarArista("v1", "v5");
+            g.insertarArista("v1", "v7");
+            g.insertarArista("v1", "v9");
+            g.insertarArista("v2", "v1");
+            g.insertarArista("v2", "v3");
+            g.insertarArista("v2", "v4");
+            g.insertarArista("v4", "v3");
+            g.insertarArista("v5", "v7");
+            g.insertarArista("v6", "v8");
+            g.insertarArista("v7", "v9");
+            g.insertarArista("v8", "v9");
+            g.insertarArista("v9", "v2");
+            System.out.println("G := (V, E), donde");
+            LinkedList<Grafica<Integer>.Vertice> vertices = g.darVertices();
+            LinkedList<Grafica<Integer>.Arista> aristas = g.darAristas();
+            int orden = g.darOrden();
+            int tamano = g.darTamano();
+            System.out.print("  V := { ");
+            int i = 1;
+            for (Grafica.Vertice v : vertices) {
+                if (i != orden) {
+                    System.out.print(v.toString() + ", ");
+                    i++;
+                } else {
+                    System.out.println(v.toString() + " }");
+                }
+            }
+            System.out.print("  E := { ");
+            int j = 1;
+            for (Grafica.Arista e : aristas) {
+                if (j != tamano) {
+                    System.out.print(e.toString() + ", ");
+                    j++;
+                } else {
+                    System.out.println(e.toString() + " }");
+                }
+            }
+            System.out.println("El orden de G es " + orden);
+            System.out.println("El tamano de G es " + tamano);
+
+            g.algoritmoDFS("v5");
+        } catch (Exception error) {
+            System.out.println(error);
+        }
+    }
+}/**Fin de la clase PruebaGrafica */
